4d3a0adf162eefc9621500fe8c762ceebd223cc9
Lukas Stateczny <statecznylukas@gmail.com>
2018-06-16 22:53:40 +0000

    getting clients ip address

--- script.js
+++ script.js
@@ -3,23 +3,37 @@ $( document ).ready(function() {
   var positive = 0;
   var negative = 0;
 
+  function getPercentageChange(p, n){
+    var decreaseValue = p - n;
+
+    return (decreaseValue / p) * 100;
+  }
+
     $('.row.secondary').hide();
 
     $('.col.left').on('click', function() {
       negative++;
       $( ".row.secondary" ).slideDown( "slow", function() {
 
+      $.get( "http://ip-api.com/json", function( data ) {
+        console.log(data);
+      });
+
       });
       $('#negative').html(negative).css('color', 'rgba(224, 86, 88)')
-      console.log(negative);
+      console.log(getPercentageChange(negative, positive));
     })
 
     $('.col.right').on('click', function() {
       positive++;
       $( ".row.secondary" ).slideDown( "slow", function() {
 
+      $.get( "http://ip-api.com/json", function( data ) {
+        console.log(data);
+      });
+
       });
       $('#positive').html(positive).css('color', 'rgba(61, 247, 102)')
-      console.log(positive);
+      console.log(getPercentageChange(positive, negative));
     })
 });
