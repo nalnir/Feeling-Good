c203cdb52c6484758fba1e7d4a08ae4781e0f8f9
Lukas Stateczny <statecznylukas@gmail.com>
2018-06-16 22:04:31 +0000

    adding simple functionality

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ $( document ).ready(function() {
 
     $('.col.left').on('click', function() {
       negative++;
-      $( ".row.secondary" ).slideToggle( "slow", function() {
+      $( ".row.secondary" ).slideDown( "slow", function() {
 
       });
+      $('#negative').html(negative).css('color', 'rgba(224, 86, 88)')
       console.log(negative);
     })
 
     $('.col.right').on('click', function() {
       positive++;
-      $( ".row.secondary" ).slideToggle( "slow", function() {
+      $( ".row.secondary" ).slideDown( "slow", function() {
 
       });
+      $('#positive').html(positive).css('color', 'rgba(61, 247, 102)')
       console.log(positive);
     })
 });
