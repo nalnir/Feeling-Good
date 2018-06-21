8df30a28acae113842c596c22a054e5a173eb886
Lukas Stateczny <statecznylukas@gmail.com>
2018-06-21 02:14:38 +0000

    getting IP addresses

--- script.js
+++ script.js
@@ -3,37 +3,64 @@ $( document ).ready(function() {
   var positive = 0;
   var negative = 0;
 
+//working on voting bar
   function getPercentageChange(p, n){
     var decreaseValue = p - n;
 
     return (decreaseValue / p) * 100;
-  }
+  };
+
+  function storeIP(data) {
+    $.ajax({
+      type: 'post',
+      url: '/',
+      data: data,
+      xhrFields: {
+          withCredentials: false
+      },
+      headers: {
+
+      },
+      success: function (data) {
+          console.log('Success');
+          console.log(data);
+      },
+      error: function () {
+          console.log('We are sorry but our servers are having an issue right now');
+      },
+    })
+  };
 
     $('.row.secondary').hide();
 
+
+
+//collecting negative feelings
     $('.col.left').on('click', function() {
-      negative++;
       $( ".row.secondary" ).slideDown( "slow", function() {
 
       $.get( "http://ip-api.com/json", function( data ) {
-        console.log(data);
+        storeIP(data.query);
+        negative++;
       });
 
       });
       $('#negative').html(negative).css('color', 'rgba(224, 86, 88)')
-      console.log(getPercentageChange(negative, positive));
+      $('#question').text('Do you want to feel better?');
     })
 
+//collecting positive feelings
     $('.col.right').on('click', function() {
       positive++;
       $( ".row.secondary" ).slideDown( "slow", function() {
 
       $.get( "http://ip-api.com/json", function( data ) {
-        console.log(data);
+        storeIP(data.query);
+        positive++;
       });
 
       });
       $('#positive').html(positive).css('color', 'rgba(61, 247, 102)')
-      console.log(getPercentageChange(positive, negative));
+      $('#question').text('Awesome!');
     })
 });
--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+//const session = require('express-session');
+const bodyParser = require('body-parser');
+
 
 app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 900000 }}))
 
-app.get('/', (req, res) => console.log(req))
+
+app.get('/', (req, res) => {
+
+  //console.log(req.query);
+  //console.log(res.statusCode);
+  //req.user = 'hardcoded';
+
+});
+
+/*
+app.get('/logout', (req, res) => {
+  console.log('logout ', req.user);
+  req.user = null;
+  console.log('logOut ', req.user)
+});
+*/
+
+app.post('/', (req, res) => console.log(req.body));
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
